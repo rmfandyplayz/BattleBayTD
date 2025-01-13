79566bcb7e84f54b1c28bda6dc2e99a0d3687b69
rmfandyplayz <58617083+rmfandyplayz@users.noreply.github.com>
2025-01-12 23:28:05 -0800

    Update 3
    funny

---
+++ Scripts/Game/GameUI.gd
@@ -0,0 +1,34 @@
+extends Node
+
+## general logic and functions for some in-game UI elements where giving them a dedicated script would be overkill
+
+
+# FYI: if "isAddition" is true, it will add to the current value instead of setting it to whatever "value" is.
+# this applies to all of these "update currency" things.
+
+@export var goldValueText : Label
+@export var goldBar : TextureProgressBar
+func UpdateGold(value : int, isAddition : bool = true):
+	if(isAddition == true):
+		goldBar.value += value
+	else:
+		goldBar.value = value
+
+@export var sugarValueText : Label
+@export var sugarBar : TextureProgressBar
+func UpdateSugar(value : int, isAddition : bool = true):
+	if(isAddition == true):
+		sugarBar.value += value
+	else:
+		sugarBar.value = value
+
+@export var pearlValueText : Label
+func UpdatePearls(value : int, isAddition : bool = true):
+	var numPearls : int = int(pearlValueText.text)
+	if(isAddition == true):
+		numPearls += value
+	else:
+		numPearls = value
+	
+	pearlValueText.text = str(numPearls)
+	
---
+++ Scripts/Game/GameUI_WaveFlag.gd
@@ -0,0 +1,4 @@
+extends Node
+
+## functions for animating the flags when they need to be raised
+## (for when a huge wave of ships are approaching)
